d251a8fdb47e9a757255b3382330b3ec7261bb27
jbytow <jakub.bytow@gmail.com>
2023-04-23 14:16:01 +0200

    0.2 scrapper works for multiple pages

--- main.py
+++ main.py
@@ -8,31 +8,43 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
+urls = ['https://glycemic-index.net/baguette-white/',
+        'https://glycemic-index.net/chocolate-bar-with-sugar/']
 
+product_list = []
+kcal_list = []
+proteins_list = []
+carbohydrates_list = []
+fats_list = []
 
-url = 'https://glycemic-index.net/baguette-white/'
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.content, "html.parser")
+for url in urls:
+    page = requests.get(url, headers=headers)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-product = soup.find('h1', class_='entry-title').text.strip()
+    product = soup.find('h1', class_='entry-title').text.strip()
 
-nutrition = soup.find("div", class_="entry-content clear", itemprop="text")
-for table in nutrition.find_all('table'):
-    table.decompose()
+    nutrition = soup.find("div", class_="entry-content clear", itemprop="text")
+    for table in nutrition.find_all('table'):
+        table.decompose()
 
-text = nutrition.get_text()
+    text = nutrition.get_text()
 
-kcal = re.search(r"(\d+)\s*kcal", text).group(1)
-proteins = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+proteins", text).group(1)
-carbohydrates = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+carbohydrates", text).group(1)
-fats = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+fats", text).group(1)
+    kcal = re.search(r"(\d+)\s*kcal", text).group(1)
+    proteins = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+proteins", text).group(1)
+    carbohydrates = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+carbohydrates", text).group(1)
+    fats = re.search(r"(\d+\.\d+)\s*grams?\s+of\s+fats", text).group(1)
 
+    product_list.append(product)
+    kcal_list.append(kcal)
+    proteins_list.append(proteins)
+    carbohydrates_list.append(carbohydrates)
+    fats_list.append(fats)
 
-df = pd.DataFrame({'Product': [product],
-                   'kcal': [kcal],
-                   'proteins': [proteins],
-                   'carbohydrates': [carbohydrates],
-                   'fats': [fats]})
+df = pd.DataFrame({'Product': product_list,
+                   'kcal': kcal_list,
+                   'proteins': proteins_list,
+                   'carbohydrates': carbohydrates_list,
+                   'fats': fats_list})
 
 df.to_csv('nutrition.csv', index=False, sep='\t')
 
